b60c7b7c1cdb4b46263b78ac115fdf9d2c1f671a
just starting <60076525+merlinpinpin41@users.noreply.github.com>
2024-03-18 18:28:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-import { Analytics } from '@vercel/analytics/react';
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
-    <Analytics />
-    <App />
-  </>
-);
-
 var swiper = new Swiper(".swiper", {
     effect: "coverflow",
     grabCursor: true,
